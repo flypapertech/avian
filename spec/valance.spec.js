30acb9d2c8d8d0876ffbb97352de34c6122a2b9b
Dan Stephenson <ispyhumanfly@gmail.com>
2017-10-01 02:21:35 -0400

    .js cleanup.

--- spec/valance.spec.js
+++
@@ -1,1 +0,0 @@
-"use strict";exports.__esModule=true;describe("Valance Distribution Files",function(){var valance;beforeEach(function(){valance.cli("./dist/valance.cli.js");valance.lib("./dist/valance.lib.js")});it("Checks to see if all distribution files have been built.",function(){})});
