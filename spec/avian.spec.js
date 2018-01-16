9836b9fc6138c55b14875b52da1d258d7184e1f9
Dan Stephenson <ispyhumanfly@gmail.com>
2018-01-16 14:32:28 -0500

    Removing 64 bit platform requirement.

--- spec/avian.spec.js
+++ spec/avian.spec.js
@@ -1,1 +1,2 @@
+#!/usr/bin/env node
 "use strict";exports.__esModule=true;var jasmine=require("jasmine");jasmine.describe("Avian Distribution Files",function(){var avian;jasmine.beforeEach(function(){avian.cli("./dist/avian.cli.js");avian.lib("./dist/avian.lib.js")});jasmine.it("Checks to see if all distribution files have been built.",function(){})});
