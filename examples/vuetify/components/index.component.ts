40c0a9a1e9801d3d28626d90ccf79811e1fd7ffe
Dan Stephenson <ispyhumanfly@gmail.com>
2018-01-18 20:36:17 -0500

    Checking in server-side object storage rendering using pug.

--- examples/vuetify/components/index.component.ts
+++ examples/vuetify/components/index.component.ts
@@ -1,2 +1,22 @@
+// Inbox Vue Component
 
-new Vue({ el: "#app" })
+new Vue(
+    {
+        el: "#app",
+        data: {
+            storage: {}
+        },
+        mounted: () => {
+            let index = this;
+            $.ajax({
+                url: "/index/storage/objects.json",
+                method: 'GET',
+                success: (objects) => {
+                    index.storage = objects;
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        }
+    })
