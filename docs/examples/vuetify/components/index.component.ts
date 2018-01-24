8900e0db14796b4997e045a77bb66aa0deea8e8c
Dan Stephenson <ispyhumanfly@gmail.com>
2018-01-24 03:31:01 -0500

    Some improvements to the vuetify example.

--- docs/examples/vuetify/components/index.component.ts
+++ docs/examples/vuetify/components/index.component.ts
@@ -2,16 +2,27 @@
 
 const Index = new Vue(
     {
-        el: "#index",
+        el: "#component",
         data: {
-            objects: {}
+            objects: {},
+            links: {}
         },
         created () {
+
+            // Retrieve All Objects Example
+
             fetch("/index/storage/objects.json")
-            .then(response =>response.json())
+            .then(response => response.json())
             .then(json => {
                 this.objects = json.objects
             })
 
+            // Retrieve Links Object
+
+            fetch("/index/storage/objects.json")
+            .then(response => response.json())
+            .then(json => {
+                this.links = json.objects
+            })
         }
     })
