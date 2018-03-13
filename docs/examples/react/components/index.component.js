cc2ee933f2dc94f10149aa3cda6dd1a5dfd7b328
Dan Stephenson <ispyhumanfly@gmail.com>
2018-03-13 14:51:52 -0400

    Closer. But not yet finished.

--- docs/examples/react/components/index.component.js
+++ docs/examples/react/components/index.component.js
@@ -1,14 +1,64 @@
-function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>The time is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
-    ReactDOM.render(
-      element,
-      document.getElementById('root')
-    );
+class Example extends React.Component {
+  constructor() {
+    super();
+    this.state = { time: {}, seconds: 60 * 60 * 24 };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
   }
 
-  setInterval(tick, 1000);
+  secondsToTime(secs){
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let obj = {
+      "h": hours,
+      "m": minutes,
+      "s": seconds
+    };
+    return obj;
+  }
+
+  componentDidMount() {
+    let timeLeftVar = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeftVar });
+  }
+
+  startTimer() {
+    if (this.timer == 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  countDown() {
+    // Remove one second, set state so a re-render happens.
+    let seconds = this.state.seconds - 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    });
+
+    // Check if we're at zero.
+    if (seconds === 0) {
+      clearInterval(this.timer);
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.startTimer}>Start</button>
+        <button>Clear</button>
+
+        h: {this.state.time.h} m: {this.state.time.m} s: {this.state.time.s}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Example/>, document.getElementById('root'));
