5278666e848947f13d0638806dc6dbbc29c46bad
logikgate <nick.fredricks@flypaper.com>
2018-07-18 12:58:52 -0400

    Modify build to only place js files in dist

--- src/avian.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-
-require("jasmine");
-
-describe("Avian Distribution Files", function() {
-    it("Checks to see if all distribution files have been built.", function() {});
-});
